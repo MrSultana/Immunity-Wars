ce7035248243ccdc30ecfa2c7b18c568054b32ff
Ethan <42900137+MrSultana@users.noreply.github.com>
2021-07-22 11:45:21 +1200

    Removed white grid object

--- ImmunityWars/Assets/Scripts/Tester.cs
+++ ImmunityWars/Assets/Scripts/Tester.cs
@@ -9,14 +9,14 @@ public class Tester : MonoBehaviour
     private Grid grid;
     private void Start()
     {
-        Grid grid = new Grid(9, 2, 2f, new Vector3(-11, 0, 17));
+        Grid grid = new Grid(8, 2, 2f, new Vector3(-11, 0, 17));
     }
 
-    private void Update()
+    /*private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
         }
-    }
+    }*/
 }
