7e43b93b9348b1bb9115f86e5f45fe76af7e78f0
Ethan <42900137+MrSultana@users.noreply.github.com>
2021-08-25 18:27:26 +1200

    jeff

--- ImmunityWars/Assets/Scripts/MovementOnGrid.cs
+++ ImmunityWars/Assets/Scripts/MovementOnGrid.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MovementOnGrid : MonoBehaviour
 {
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0)) {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);// code taken from https://answers.unity.com/questions/1386777/get-origin-of-plane-in-world-coordinates.html
+            var hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit)) {
+                Debug.Log(hit.point);
+
+                Instantiate(target, hit.point, target.rotation);
+            }
+            
+        }
+
     }
 }
